29a065446c398a117898ff72ad6f49c27064b59c
RomanSibakovskij <j4023958@gmail.com>
2024-09-20 17:13:46 +0300

    Add TestDataGenerator class

---
+++ src/main/java/com/applitools/demo/TestDataGenerator.java
@@ -0,0 +1,67 @@
+package com.applitools.demo;
+
+import org.openqa.selenium.WebDriver;
+import java.security.SecureRandom;
+import java.util.*;
+
+public class TestDataGenerator extends BasePage{
+
+    //login data
+    private static String username;
+    private static String password;
+
+    //randomizer variables
+    private static final Random RANDOM = new SecureRandom();
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+
+    public TestDataGenerator(WebDriver driver) {
+        super(driver);
+    }
+
+    // random string methods generator
+    private static String generateRandomString(String characters, int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = RANDOM.nextInt(characters.length());
+            sb.append(characters.charAt(index));
+        }
+        return sb.toString();
+    }
+
+    private static String shuffleString(String string) {
+        List<String> letters = Arrays.asList(string.split(""));
+        Collections.shuffle(letters);
+        return String.join("", letters);
+    }
+
+    // randomizer method
+    public static String generateRandomUsername(int length) {
+        return generateRandomString(UPPER + LOWER, length);
+    }
+
+    //random password generator
+    public static String generateRandomPassword() {
+        String numbers = "0123456789";
+
+        // fixed password part
+        String stokerPart = "Stoker";
+
+        // random numeric sequence
+        StringBuilder numericPart = new StringBuilder();
+        numericPart.append(numbers.charAt(RANDOM.nextInt(numbers.length())));
+        numericPart.append(numbers.charAt(RANDOM.nextInt(numbers.length())));
+        numericPart.append(numbers.charAt(RANDOM.nextInt(numbers.length())));
+        numericPart.append('_'); // Static underscore
+
+        // shuffle the numeric part
+        String shuffledNumericPart = shuffleString(numericPart.toString());
+
+        //"Stoker" + shuffled numeric part
+        StringBuilder password = new StringBuilder();
+        password.append(stokerPart);
+        password.append(shuffledNumericPart);
+
+        return password.toString();
+    }
+}
